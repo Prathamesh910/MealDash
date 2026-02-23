de990cf016b39d3b6c0d2f13508969dca7397163
Prathamesh910 <prathameshgiradkar910@gmail.com>
2026-02-23 19:18:45 +0530

    completed testing part 1...

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -6,7 +6,7 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
 // placing user order for frontend
 const placeOrder = async (req, res) => {
-  const frontend_url = "http://localhost:5173";
+  const frontend_url = "http://localhost:5174";
 
   try {
     const newOrder = new orderModel({
@@ -95,11 +95,13 @@ const listOrders = async (req, res) => {
 // api for updating order status
 const updateStatus = async (req, res) => {
   try {
-    await orderModel.findByIdAndUpdate(req.body.orderId,{status:req.body.status})
-    res.json({success:true,message:"Status Update"})
+    await orderModel.findByIdAndUpdate(req.body.orderId, {
+      status: req.body.status,
+    });
+    res.json({ success: true, message: "Status Update" });
   } catch (error) {
-    console.log(error)
-    res.json({success:false,message:"Error"})
+    console.log(error);
+    res.json({ success: false, message: "Error" });
   }
 };
 
