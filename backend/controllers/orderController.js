221a6cd03c91937057ea032ef593092e8c985e36
Prathamesh910 <prathameshgiradkar910@gmail.com>
2026-02-23 18:21:38 +0530

    Created frontend for order page of admin panel...

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -72,15 +72,24 @@ const verifyOrder = async (req, res) => {
 
 // user orders for frontend
 const userOrders = async (req, res) => {
-  
   try {
-    
     const orders = await orderModel.find({ userId: req.body.userId });
-    res.json({ success: true, data:orders });
+    res.json({ success: true, data: orders });
   } catch (error) {
     console.log(error);
     res.json({ success: false, message: "Error" });
   }
 };
 
-export { placeOrder, verifyOrder, userOrders };
+// listing orders for admin pannel
+const listOrders = async (req, res) => {
+  try {
+    const orders = await orderModel.find({});
+    res.json({ success: true, data: orders });
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: "Error" });
+  }
+};
+
+export { placeOrder, verifyOrder, userOrders, listOrders };
